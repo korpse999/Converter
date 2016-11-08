fb0d46e573aed26301c70d89f88cb8be797e5d36
korpse999 <zexav1@gmail.com>
2016-11-08 15:06:50 +0100

    ajout test

--- src/test/java/converter/TemperatureTest.java
+++ src/test/java/converter/TemperatureTest.java
@@ -16,18 +16,26 @@ import converter.Temperature;
 public class TemperatureTest {
 
 	/**
-	 * Test method for {@link converter.Temperature#conversionC()}.
-	 * On vérifie que la conversion F --> C fonctionne correctement
-	 * - la valeur numérique de la nouvelle temperature est celle attendue
-	 * - l'unité est celle attendue
+	 * Test method for {@link converter.Temperature#conversionC()}. On vérifie
+	 * que la conversion F --> C fonctionne correctement - la valeur numérique
+	 * de la nouvelle temperature est celle attendue - l'unité est celle
+	 * attendue
 	 */
 	@Test
 	public void testConversionC() {
-		Temperature TempF = new Temperature (32.0 , 'F');
+		Temperature TempF = new Temperature(32.0, 'F');
 		Temperature TempC = TempF.conversionC();
 		assertEquals(TempC.getTemperature(), 0.0, 0.1);
-		assertEquals(TempC.getUniteDeMesure(),'C');
-		//fail("Not yet implemented");
+		assertEquals(TempC.getUniteDeMesure(), 'C');
+
+		// fail("Not yet implemented");
+	}
+
+	public void testConversionF() {
+		Temperature TempC1 = new Temperature(-40.0, 'C');
+		Temperature TempF1 = TempC1.conversionF();
+		assertEquals(TempF1.getTemperature(), -40.0, 0.1);
+		assertEquals(TempF1.getUniteDeMesure(), 'F');
 	}
 
 }
