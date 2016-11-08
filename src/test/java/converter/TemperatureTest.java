e6c76bba6e53101f64849c4372a8e6635bef80b0
korpse999 <zexav1@gmail.com>
2016-11-08 16:49:06 +0100

    avec des tests en plus

--- src/test/java/converter/TemperatureTest.java
+++ src/test/java/converter/TemperatureTest.java
@@ -25,17 +25,46 @@ public class TemperatureTest {
 	public void testConversionC() {
 		Temperature TempF = new Temperature(32.0, 'F');
 		Temperature TempC = TempF.conversionC();
-		assertEquals(TempC.getTemperature(), 0.0, 0.1);
-		assertEquals(TempC.getUniteDeMesure(), 'C');
-
+		assertEquals(0.0 , TempC.getTemperature(), 0.1);
+		assertEquals('C' , TempC.getUniteDeMesure() );
 		// fail("Not yet implemented");
 	}
-
+	@Test
 	public void testConversionF() {
-		Temperature TempC1 = new Temperature(-40.0, 'C');
-		Temperature TempF1 = TempC1.conversionF();
-		assertEquals(TempF1.getTemperature(), -40.0, 0.1);
-		assertEquals(TempF1.getUniteDeMesure(), 'F');
+		Temperature TempC = new Temperature(-40.0, 'C');
+		Temperature TempF = TempC.conversionF();
+		assertEquals(-40 , TempF.getTemperature(), 0.1);
+		assertEquals('F' , TempF.getUniteDeMesure());
 	}
-
+	@Test
+	public void testConversionC2() {
+		Temperature TempF = new Temperature(-40, 'F');
+		Temperature TempC = TempF.conversionC();
+		assertEquals(-40 , TempC.getTemperature(), 0.1);
+		assertEquals('C' , TempC.getUniteDeMesure());
+	}
+	@Test
+	public void testConversionC3() {
+		Temperature TempF = new Temperature(64, 'F');
+		Temperature TempC = TempF.conversionC();
+		assertEquals(17.78 , TempC.getTemperature(), 0.01);
+		assertEquals('C' , TempC.getUniteDeMesure());
+	}
+	
+	@Test
+	public void testConversionReverse(){
+		Temperature TempC = new Temperature(32 , 'C');
+		Temperature TempF = TempC.conversionF();
+		
+		Temperature TempX = TempF.conversionC();
+		
+		assertEquals(TempC.getTemperature() , TempX.getTemperature(), 0.1);
+		assertEquals(TempC.getUniteDeMesure(), TempX.getUniteDeMesure());
+		
+		assertEquals(89.6, TempF.getTemperature() ,0.1);
+		assertEquals('F' , TempF.getUniteDeMesure());
+		
+	}
+	
+	
 }
